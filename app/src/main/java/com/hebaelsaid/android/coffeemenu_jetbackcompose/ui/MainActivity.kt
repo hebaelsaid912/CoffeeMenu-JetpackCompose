5a94199cecbe7d04bf7d1fb72e93c81e2ecc3708
Heba Elsaid <hebaelsaid912@gmail.com>
2022-12-01 18:12:37 +0200

    handle network connectivity changes

--- app/src/main/java/com/hebaelsaid/android/coffeemenu_jetbackcompose/ui/MainActivity.kt
+++ app/src/main/java/com/hebaelsaid/android/coffeemenu_jetbackcompose/ui/MainActivity.kt
@@ -1,29 +1,37 @@
 package com.hebaelsaid.android.coffeemenu_jetbackcompose.ui
 
 import android.os.Bundle
-import android.os.Parcelable
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.hebaelsaid.android.coffeemenu_jetbackcompose.data.model.responsemodel.CoffeeResponseModel
 import com.hebaelsaid.android.coffeemenu_jetbackcompose.ui.features.details.component.CoffeeDetailsScreen
 import com.hebaelsaid.android.coffeemenu_jetbackcompose.ui.features.home.component.*
 import com.hebaelsaid.android.coffeemenu_jetbackcompose.ui.features.onboarding.component.OnBoardingScreen
 import com.hebaelsaid.android.coffeemenu_jetbackcompose.ui.theme.CoffeeMenuJetbackComposeTheme
+import com.hebaelsaid.android.coffeemenu_jetbackcompose.utils.ConnectionState
+import com.hebaelsaid.android.coffeemenu_jetbackcompose.utils.currentConnectivityState
+import com.hebaelsaid.android.coffeemenu_jetbackcompose.utils.observeConnectivityAsFlow
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.parcelize.Parcelize
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 private const val TAG = "MainActivity"
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -34,6 +42,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     SetupAppRouteNavigation()
+                    ConnectivityStatus()
                 }
             }
         }
@@ -51,25 +60,44 @@ class MainActivity : ComponentActivity() {
                 Log.d(TAG, "SetupAppRouteNavigation: HomeScreen")
                 HomeScreen(navController)
             }
-            composable(route = Screen.OnBoardingScreen.route + "/${Screen.CoffeeDetailsScreen.route}"/*+ "/{${Constant.PARAM_COFFEE_MODEL}}"*//*,listOf(navArgument(
-                PARAM_COFFEE_MODEL) {
-                type = NavType.ParcelableType<CoffeeResponseModel.CoffeeResponseModelItem>(CoffeeResponseModel.CoffeeResponseModelItem::class.java)
-            })*/) { navBackStackEntry ->
+            composable(route = Screen.OnBoardingScreen.route + "/${Screen.CoffeeDetailsScreen.route}") { navBackStackEntry ->
                 Log.d(TAG, "SetupAppRouteNavigation: CoffeeDetailsScreen")
-               /* val model = navBackStackEntry.arguments?.getParcelable<CoffeeResponseModel.CoffeeResponseModelItem>(PARAM_COFFEE_MODEL)
-                if (model != null) {
-                    CoffeeDetailsScreen(model)
-                }*/
                 CoffeeDetailsScreen()
             }
             
         }
     }
-    @Parcelize
-    sealed class Destination : Parcelable {
-        object OnBoarding : Destination()
-        object Home : Destination()
-        data class CoffeeDetails(val name: String, val coffeeDetails: CoffeeResponseModel.CoffeeResponseModelItem) : Destination()
+    @ExperimentalCoroutinesApi
+    @Composable
+    fun connectivityState(): State<ConnectionState> {
+        val context = LocalContext.current
+        // Creates a State<ConnectionState> with current connectivity state as initial value
+        return produceState(initialValue = context.currentConnectivityState) {
+            // In a coroutine, can make suspend calls
+            context.observeConnectivityAsFlow().collect { value = it }
+        }
     }
+    @ExperimentalCoroutinesApi
+    @Composable
+    fun ConnectivityStatus() {
+        // This will cause re-composition on every network state change
+        val connection by connectivityState()
 
+        val isConnected = connection === ConnectionState.Available
+
+        if (!isConnected) {
+           /* Snackbar(
+                action = {
+                    Button(onClick = {}) {
+                        Text("check internet")
+                    }
+                },
+                modifier = Modifier.fillMaxWidth().padding(8.dp).height(20.dp).background(Color.Red)
+            ) { Text(text = "You are offline") }*/
+        } else {
+           /* Snackbar(
+                modifier = Modifier.fillMaxWidth().padding(8.dp).height(20.dp).background(Color.Green)
+            ) { Text(text = "Back online") }*/
+        }
+    }
 }
---
+++ app/src/main/java/com/hebaelsaid/android/coffeemenu_jetbackcompose/utils/NetworkUtil.kt
@@ -0,0 +1,68 @@
+package com.hebaelsaid.android.coffeemenu_jetbackcompose.utils
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.callbackFlow
+
+val Context.currentConnectivityState: ConnectionState
+    get() {
+        val connectivityManager =
+            getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        return getCurrentConnectivityState(connectivityManager)
+    }
+
+private fun getCurrentConnectivityState(
+    connectivityManager: ConnectivityManager
+): ConnectionState {
+    val connected = connectivityManager.allNetworks.any { network ->
+        connectivityManager.getNetworkCapabilities(network)
+            ?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+            ?: false
+    }
+
+    return if (connected) ConnectionState.Available else ConnectionState.Unavailable
+}
+sealed class ConnectionState {
+    object Available : ConnectionState()
+    object Unavailable : ConnectionState()
+}
+
+@ExperimentalCoroutinesApi
+fun Context.observeConnectivityAsFlow() = callbackFlow {
+    val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+    val callback = networkCallback { connectionState -> trySend(connectionState) }
+
+    val networkRequest = NetworkRequest.Builder()
+        .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+        .build()
+
+    connectivityManager.registerNetworkCallback(networkRequest, callback)
+
+    // Set current state
+    val currentState = getCurrentConnectivityState(connectivityManager)
+    trySend(currentState)
+
+    // Remove callback when not used
+    awaitClose {
+        // Remove listeners
+        connectivityManager.unregisterNetworkCallback(callback)
+    }
+}
+
+fun networkCallback(callback: (ConnectionState) -> Unit): ConnectivityManager.NetworkCallback {
+    return object : ConnectivityManager.NetworkCallback() {
+        override fun onAvailable(network: Network) {
+            callback(ConnectionState.Available)
+        }
+
+        override fun onLost(network: Network) {
+            callback(ConnectionState.Unavailable)
+        }
+    }
+}
