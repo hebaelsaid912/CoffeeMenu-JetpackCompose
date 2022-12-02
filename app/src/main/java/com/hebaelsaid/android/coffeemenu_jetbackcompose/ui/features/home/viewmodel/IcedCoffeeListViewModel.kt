5047d2cb56fd7678edb77fd5364264120bc94f6c
Heba Elsaid <hebaelsaid912@gmail.com>
2022-12-02 13:18:32 +0200

    handle database insertion into iced coffee list table

--- app/src/main/java/com/hebaelsaid/android/coffeemenu_jetbackcompose/ui/features/home/viewmodel/IcedCoffeeListViewModel.kt
+++ app/src/main/java/com/hebaelsaid/android/coffeemenu_jetbackcompose/ui/features/home/viewmodel/IcedCoffeeListViewModel.kt
@@ -4,6 +4,9 @@ import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.hebaelsaid.android.coffeemenu_jetbackcompose.data.local.database.CoffeeMenuDatabase
+import com.hebaelsaid.android.coffeemenu_jetbackcompose.data.local.entities.IcedCoffeeDetailsItem
+import com.hebaelsaid.android.coffeemenu_jetbackcompose.data.model.responsemodel.CoffeeResponseModel
 import com.hebaelsaid.android.coffeemenu_jetbackcompose.domain.usecase.coffeelist.GetIcedCoffeeListUseCase
 import com.hebaelsaid.android.coffeemenu_jetbackcompose.ui.features.home.state.CoffeeListState
 import com.hebaelsaid.android.coffeemenu_jetbackcompose.utils.Resource
@@ -14,7 +17,8 @@ import javax.inject.Inject
 
 @HiltViewModel
 class IcedCoffeeListViewModel @Inject constructor(
-    private val getIcedCoffeeUseCase: GetIcedCoffeeListUseCase
+    private val getIcedCoffeeUseCase: GetIcedCoffeeListUseCase,
+    private val coffeeMenuDatabase: CoffeeMenuDatabase
 ): ViewModel(){
     private val _state  = mutableStateOf(CoffeeListState())
     val state: State<CoffeeListState> = _state
@@ -28,6 +32,7 @@ class IcedCoffeeListViewModel @Inject constructor(
                 is Resource.Success ->{
                     if(!resultState.data.isNullOrEmpty()) {
                         _state.value = CoffeeListState(modelItem = resultState.data)
+                        insertIcedListItemsIntoDB(resultState.data)
                     }else{
                         _state.value = CoffeeListState(error ="Data Return With Null")
                     }
@@ -40,5 +45,24 @@ class IcedCoffeeListViewModel @Inject constructor(
                 }
             }
         }.launchIn(viewModelScope)
+    }
+    private suspend fun insertIcedListItemsIntoDB(data: CoffeeResponseModel) {
+        clearIcedCoffeeTable()
+        for (item in data) {
+            coffeeMenuDatabase.coffeeMenuDao().insertIcedCoffeeMenuItem(
+                IcedCoffeeDetailsItem(
+                    description = item.description,
+                    item_id = item.id,
+                    image = item.image,
+                    title = item.title,
+                    ingredients = item.ingredients,
+                    type = "hot"
+                )
+            )
+        }
+    }
+
+    private suspend fun clearIcedCoffeeTable() {
+        coffeeMenuDatabase.coffeeMenuDao().clearIcedCoffeeMenuDatabase()
     }
 }
