15f2cf0028898af95d4afeaac78ad7275d1ff7d7
Heba Elsaid <hebaelsaid912@gmail.com>
2022-12-02 13:12:28 +0200

    handle iced coffee on click navigation

--- app/src/main/java/com/hebaelsaid/android/coffeemenu_jetbackcompose/ui/features/home/component/IcedCoffeeScreen.kt
+++ app/src/main/java/com/hebaelsaid/android/coffeemenu_jetbackcompose/ui/features/home/component/IcedCoffeeScreen.kt
@@ -1,9 +1,7 @@
 package com.hebaelsaid.android.coffeemenu_jetbackcompose.ui.features.home.component
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Divider
@@ -12,42 +10,63 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.hebaelsaid.android.coffeemenu_jetbackcompose.R
+import com.hebaelsaid.android.coffeemenu_jetbackcompose.ui.Screen
 import com.hebaelsaid.android.coffeemenu_jetbackcompose.ui.features.home.viewmodel.IcedCoffeeListViewModel
+import com.hebaelsaid.android.coffeemenu_jetbackcompose.utils.connectivityStatus
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 
+@OptIn(ExperimentalCoroutinesApi::class)
 @Composable
 fun IcedCoffeeScreen(
     navController: NavController,
     viewModel: IcedCoffeeListViewModel = hiltViewModel()
 ) {
     val state = viewModel.state.value
-    Box(modifier = Modifier.fillMaxSize().padding(15.dp)) {
-        LazyColumn(modifier = Modifier
-            .fillMaxSize()
-            .padding(10.dp)){
-                items(state.modelItem.size){ coffeeModels ->
-                    CoffeeListItem(model = state.modelItem[coffeeModels], onItemClick ={
-                        // navController.navigate(Screen.CoinDetailsScreen.route + "/${coffeeModel.coin_id}")
-                    } )
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .padding(15.dp)) {
+        if(connectivityStatus()) {
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(10.dp)
+            ) {
+                items(state.modelItem.size) { coffeeModels ->
+                    CoffeeListItem(model = state.modelItem[coffeeModels], onItemClick = {
+                        navController.navigate(Screen.OnBoardingScreen.route + "/${Screen.CoffeeDetailsScreen.route}")
+                    })
                     Divider()
                 }
-        }
-        if(state.error.isNotBlank()){
-            Text(
-                text = state.error,
-                color = MaterialTheme.colors.error,
-                textAlign = TextAlign.Center,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 20.dp)
-                    .align(Alignment.Center)
-            )
-        }
-        if(state.isLoading){
-            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+            }
+            if (state.error.isNotBlank()) {
+                Text(
+                    text = state.error,
+                    color = MaterialTheme.colors.error,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 20.dp)
+                        .align(Alignment.Center)
+                )
+            }
+            if (state.isLoading) {
+                CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+            }
+        }else{
+            Column(
+                Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Image(painter = painterResource(id = R.drawable.network) , contentDescription = "no internet connection")
+                Text(text = "No Internet Connection!")
+            }
         }
     }
 }
